5b1e03e5a601e148f32c4a8f3d97c8318c14ca11
anislouail02-ux <anis.louail02@gmail.com>
2026-04-04 00:04:05 +0200

    generate_traffic.py

--- generate_traffic.py
+++ generate_traffic.py
@@ -6,14 +6,14 @@ flag_encoded = base64.b64encode(FLAG.encode()).decode()
 
 packets = []
 
-# --- Normal traffic (decoy) ---
+
 for i in range(5):
     pkt = (IP(src="192.168.1.10", dst="93.184.216.34") /
            TCP(dport=80, flags="S") /
            Raw(load=f"GET /page{i} HTTP/1.1\r\nHost: example.com\r\n\r\n"))
     packets.append(pkt)
 
-# --- Suspicious packet with hidden flag ---
+
 sus_pkt = (IP(src="192.168.1.10", dst="10.10.10.99") /
            TCP(dport=80, flags="S") /
            Raw(load=f"GET /upload HTTP/1.1\r\n"
@@ -21,7 +21,7 @@ sus_pkt = (IP(src="192.168.1.10", dst="10.10.10.99") /
                     f"X-Data: {flag_encoded}\r\n\r\n"))
 packets.append(sus_pkt)
 
-# --- More decoy traffic ---
+
 for i in range(4):
     pkt = (IP(src="192.168.1.10", dst="93.184.216.34") /
            TCP(dport=80, flags="S") /
